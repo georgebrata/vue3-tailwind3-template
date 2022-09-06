32f77e0ce55bda477dc8c5c551757ad606d0ee59
George Brata <bratageorge@gmail.com>
2022-09-06 09:43:58 +0300

    added initial files 🚀

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="p-14 ">
+    <router-link class="flex justify-center text-4xl text-red-700" to="/"
+      >Vue3 Tailwind3 Boilerplate</router-link
+    >
+    <router-view />
+  </div>
+</template>
+
+<style></style>
